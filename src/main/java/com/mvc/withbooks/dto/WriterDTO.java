381fc859cb6bf35ebdcfca8d31baa701c8da419f
Bombbbbb <creamcake5388@gmail.com>
2022-06-07 16:48:47 +0900

    writer DTO

---
+++ src/main/java/com/mvc/withbooks/dto/WriterDTO.java
@@ -0,0 +1,78 @@
+package com.mvc.withbooks.dto;
+
+public class WriterDTO {
+	private int Wnum;
+	private String Writer_name;
+	private String Writer_id;
+	private String Writer_passwd;
+	private String Writer_tel;
+	private String Writer_address;
+	private String Writer_email;
+	private String Writer_birth;
+	private String Writer_sex;
+	private String Writer_preferred;
+	
+	
+	public int getWnum() {
+		return Wnum;
+	}
+	public void setWnum(int wnum) {
+		Wnum = wnum;
+	}
+	public String getWriter_name() {
+		return Writer_name;
+	}
+	public void setWriter_name(String writer_name) {
+		Writer_name = writer_name;
+	}
+	public String getWriter_id() {
+		return Writer_id;
+	}
+	public void setWriter_id(String writer_id) {
+		Writer_id = writer_id;
+	}
+	public String getWriter_passwd() {
+		return Writer_passwd;
+	}
+	public void setWriter_passwd(String writer_passwd) {
+		Writer_passwd = writer_passwd;
+	}
+	public String getWriter_tel() {
+		return Writer_tel;
+	}
+	public void setWriter_tel(String writer_tel) {
+		Writer_tel = writer_tel;
+	}
+	public String getWriter_address() {
+		return Writer_address;
+	}
+	public void setWriter_address(String writer_address) {
+		Writer_address = writer_address;
+	}
+	public String getWriter_email() {
+		return Writer_email;
+	}
+	public void setWriter_email(String writer_email) {
+		Writer_email = writer_email;
+	}
+	public String getWriter_birth() {
+		return Writer_birth;
+	}
+	public void setWriter_birth(String writer_birth) {
+		Writer_birth = writer_birth;
+	}
+	public String getWriter_sex() {
+		return Writer_sex;
+	}
+	public void setWriter_sex(String writer_sex) {
+		Writer_sex = writer_sex;
+	}
+	public String getWriter_preferred() {
+		return Writer_preferred;
+	}
+	public void setWriter_preferred(String writer_preferred) {
+		Writer_preferred = writer_preferred;
+	}
+	
+	
+}

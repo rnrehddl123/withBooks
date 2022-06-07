8dad6e9048d135c37278c59a62fc71a44ed8d464
I7A-27\i7C-04 <tkddn3901@gmail.com>
2022-06-07 16:58:14 +0900

    jsp생성

---
+++ src/main/java/com/mvc/withbooks/dto/MemberClientDTO.java
@@ -0,0 +1,75 @@
+package com.mvc.withbooks.dto;
+
+public class MemberClientDTO {
+	private int cnum;
+	private String client_name;
+	private String client_id;
+	private String client_passwd;
+	private String client_tel;
+	private String client_address;
+	private String client_email;
+	private String client_birth;
+	private String client_sex;
+	private String client_preferred;
+	public int getCnum() {
+		return cnum;
+	}
+	public void setCnum(int cnum) {
+		this.cnum = cnum;
+	}
+	public String getClient_name() {
+		return client_name;
+	}
+	public void setClient_name(String client_name) {
+		this.client_name = client_name;
+	}
+	public String getClient_id() {
+		return client_id;
+	}
+	public void setClient_id(String client_id) {
+		this.client_id = client_id;
+	}
+	public String getClient_passwd() {
+		return client_passwd;
+	}
+	public void setClient_passwd(String client_passwd) {
+		this.client_passwd = client_passwd;
+	}
+	public String getClient_tel() {
+		return client_tel;
+	}
+	public void setClient_tel(String client_tel) {
+		this.client_tel = client_tel;
+	}
+	public String getClient_address() {
+		return client_address;
+	}
+	public void setClient_address(String client_address) {
+		this.client_address = client_address;
+	}
+	public String getClient_email() {
+		return client_email;
+	}
+	public void setClient_email(String client_email) {
+		this.client_email = client_email;
+	}
+	public String getClient_birth() {
+		return client_birth;
+	}
+	public void setClient_birth(String client_birth) {
+		this.client_birth = client_birth;
+	}
+	public String getClient_sex() {
+		return client_sex;
+	}
+	public void setClient_sex(String client_sex) {
+		this.client_sex = client_sex;
+	}
+	public String getClient_preferred() {
+		return client_preferred;
+	}
+	public void setClient_preferred(String client_preferred) {
+		this.client_preferred = client_preferred;
+	}
+	
+}

a22050f1d08a36596fc6b4b83e3d832fb3f9999c
DESKTOP-I8QKS04\i7C-43 <greenmwo@gmail.com>
2022-06-07 14:38:25 +0900

    BookDTO - 이송주

--- src/main/java/com/mvc/withbooks/book/dto/BookDTO.java
+++ src/main/java/com/mvc/withbooks/book/dto/BookDTO.java
@@ -1,5 +1,48 @@
 package com.mvc.withbooks.book.dto;
 
 public class BookDTO {
-
+	private int Nnum;
+	private String Novel_subject;
+	private String Novel_writerName;
+	private String Novel_image;
+	private String Novel_cate;
+	private String Novel_summary;
+	public int getNnum() {
+		return Nnum;
+	}
+	public void setNnum(int nnum) {
+		Nnum = nnum;
+	}
+	public String getNovel_subject() {
+		return Novel_subject;
+	}
+	public void setNovel_subject(String novel_subject) {
+		Novel_subject = novel_subject;
+	}
+	public String getNovel_writerName() {
+		return Novel_writerName;
+	}
+	public void setNovel_writerName(String novel_writerName) {
+		Novel_writerName = novel_writerName;
+	}
+	public String getNovel_image() {
+		return Novel_image;
+	}
+	public void setNovel_image(String novel_image) {
+		Novel_image = novel_image;
+	}
+	public String getNovel_cate() {
+		return Novel_cate;
+	}
+	public void setNovel_cate(String novel_cate) {
+		Novel_cate = novel_cate;
+	}
+	public String getNovel_summary() {
+		return Novel_summary;
+	}
+	public void setNovel_summary(String novel_summary) {
+		Novel_summary = novel_summary;
+	}
+	
+	
 }

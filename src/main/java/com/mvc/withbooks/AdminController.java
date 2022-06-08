d221cea652f93404b7a68f4edb1cac921ab21996
I7A-27\i7C-04 <tkddn3901@gmail.com>
2022-06-08 14:17:10 +0900

    AdminController, LoginController 추가

---
+++ src/main/java/com/mvc/withbooks/AdminController.java
@@ -0,0 +1,40 @@
+package com.mvc.withbooks;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+	
+	@RequestMapping("/cateManage")
+	public String cateManage() {
+		return "admin/cateManage";
+	}
+	
+	@RequestMapping("/notice")
+	public String notice() {
+		return "admin/notice";
+	}
+	
+	@RequestMapping("/clientManage")
+	public String clientManage() {
+		return "admin/clientManage";
+	}
+	
+	@RequestMapping("/writerManage")
+	public String writerManage() {
+		return "admin/writerManage";
+	}
+	
+	@RequestMapping("/novelManage")
+	public String novelManage() {
+		return "admin/novelManage";
+	}
+	
+	@RequestMapping("/saleManage")
+	public String saleManage() {
+		return "admin/saleManage";
+	}
+
+}

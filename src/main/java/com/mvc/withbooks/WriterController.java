e6f3d2782a10b8b900d2fc8b877291b30273f8e5
I7A-32\i7C-04 <zip00901@naver.com>
2022-06-10 16:48:33 +0900

    no message

--- src/main/java/com/mvc/withbooks/WriterController.java
+++ src/main/java/com/mvc/withbooks/WriterController.java
@@ -4,13 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.mvc.withbooks.service.NovelMapper;
 
 @Controller
 public class WriterController {
 	
-	@Autowired
-	private NovelMapper novelMapper;
 	
 	@RequestMapping("/writerOrderList")
 	public String WriterOrderList() {

7677fe8b0ac3a6ff172b9770fa6f0b0ea0ba4e4d
I7A-32\i7C-04 <zip00901@naver.com>
2022-06-10 14:28:09 +0900

    board / notice dto

---
+++ src/main/java/com/mvc/withbooks/dto/NoticeDTO.java
@@ -0,0 +1,35 @@
+package com.mvc.withbooks.dto;
+
+public class NoticeDTO {
+	private int Nonum;
+	private String Notice_title;
+	private String Notice_content;
+	private String Notice_reg_date;
+	
+	public int getNonum() {
+		return Nonum;
+	}
+	public void setNonum(int nonum) {
+		Nonum = nonum;
+	}
+	public String getNotice_title() {
+		return Notice_title;
+	}
+	public void setNotice_title(String notice_title) {
+		Notice_title = notice_title;
+	}
+	public String getNotice_content() {
+		return Notice_content;
+	}
+	public void setNotice_content(String notice_content) {
+		Notice_content = notice_content;
+	}
+	public String getNotice_reg_date() {
+		return Notice_reg_date;
+	}
+	public void setNotice_reg_date(String notice_reg_date) {
+		Notice_reg_date = notice_reg_date;
+	}
+	
+	
+}

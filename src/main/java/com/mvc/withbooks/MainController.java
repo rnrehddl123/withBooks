d335684ccace16e577c776eabe0eaa35b76b88bb
I7A-27\i7C-04 <tkddn3901@gmail.com>
2022-06-15 16:49:50 +0900

    메인페이지 슬라이드 수정

--- src/main/java/com/mvc/withbooks/MainController.java
+++ src/main/java/com/mvc/withbooks/MainController.java
@@ -1,14 +1,28 @@
 package com.mvc.withbooks;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mvc.withbooks.dto.AdminSlideDTO;
+import com.mvc.withbooks.service.AdminSlideMapper;
 
 @Controller
 public class MainController {
 	
+	@Autowired
+	private AdminSlideMapper adminSlideMapper;
 	
 	@RequestMapping("/main")
-	public String main() {
+	public String main(HttpServletRequest req) {
+		List<AdminSlideDTO> list = adminSlideMapper.listSlide();
+		req.setAttribute("listSlide", list);
+		System.out.print("리스트 : " + list.get(0).getSlidet_image());
+		
 		return "/main/main";
 	}
 	

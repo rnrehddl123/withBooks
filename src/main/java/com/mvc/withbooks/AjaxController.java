65e76caa559961620d68b1311affea9bb990bf56
DESKTOP-I8QKS04\i7C-43 <greenmwo@gmail.com>
2022-06-28 16:47:11 +0900

    이송주

--- src/main/java/com/mvc/withbooks/AjaxController.java
+++ src/main/java/com/mvc/withbooks/AjaxController.java
@@ -92,6 +92,11 @@ public class AjaxController {
 		}else {
 			params.put("Purchase_price", "100");
 		}
+		if(login.getCash()<100) {
+			req.setAttribute("msg", "보유 포인트가 부족합니다. 포인트 충전 사이트로 이동합니다.");
+			req.setAttribute("url", "clientPay");
+			return "message";
+		}
 		Map<String, String> epmap = episodeMapper.contentNoEpisode(Integer.parseInt(params.get("epnum")));
 		int res = memberMapper.purchaseCash(params);
 		params.put("nnum", String.valueOf(epmap.get("NNUM")));
